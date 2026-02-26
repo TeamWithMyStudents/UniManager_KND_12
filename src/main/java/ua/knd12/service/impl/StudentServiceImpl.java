8c2a539d715288e8157dbaf20f0965921f0403a9
anzwkk <anam89737@gmail.com>
2026-02-26 20:20:39 +0200

    Override findByGroup method from StudentService

--- src/main/java/ua/knd12/service/impl/StudentServiceImpl.java
+++ src/main/java/ua/knd12/service/impl/StudentServiceImpl.java
@@ -2,9 +2,19 @@ package ua.knd12.service.impl;
 
 import ua.knd12.model.Student;
 import ua.knd12.model.User;
+import ua.knd12.service.StudentService;
 
-public class StudentServiceImpl extends UserServiceImpl {
-    public StudentServiceImpl() {
-        super( new Student[10]);
+public class StudentServiceImpl extends UserServiceImpl implements StudentService {
+    public StudentServiceImpl()
+    {
+        super(new Student[10]);
+    }
+
+    @Override
+    public void findByGroup(String groupName) {
+
     }
 }
+
+
+
