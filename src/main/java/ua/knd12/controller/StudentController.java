31da9bbd4316e29310bc2ec006c7b3dd143e036d
MAFIOZZZO67 <mafiozzzo502@gmail.com>
2026-02-28 14:48:41 +0200

    Made a method for adding elements to array

--- src/main/java/ua/knd12/controller/StudentController.java
+++ src/main/java/ua/knd12/controller/StudentController.java
@@ -1,4 +1,27 @@
 package ua.knd12.controller;
 
+import ua.knd12.model.User;
+
+import java.util.Arrays;
+
 public class StudentController {
+    protected User[]initialArray;
+
+    public StudentController(User[] initialArray) {
+        this.initialArray = initialArray;
+    }
+
+
+    public void add(User u) {
+        int counter = 0;
+        int newSize = (initialArray.length*2)+1;
+        if (initialArray.length<=counter) {
+            initialArray = Arrays.copyOf(initialArray, newSize);
+        }
+        initialArray[counter] = u;
+        counter++;
+    }
+
+    public void delete(int id) {
+    }
 }
--- src/main/java/ua/knd12/service/impl/UserServiceImpl.java
+++ src/main/java/ua/knd12/service/impl/UserServiceImpl.java
@@ -1,21 +1,5 @@
 package ua.knd12.service.impl;
-
-import ua.knd12.model.User;
 import ua.knd12.service.UserService;
 
-import java.util.Arrays;
-
-
 abstract class UserServiceImpl implements UserService {
-    protected User[]initialArray;
-
-    public UserServiceImpl(User[] initialArray) {
-        this.initialArray = initialArray;
-    }
-
-    public void add(User u){
-    }
-    public void delete(int id){
-
-    }
 }
