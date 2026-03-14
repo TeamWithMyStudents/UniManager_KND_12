2ef5774116d866d4c082030a021b22504daebfc4
nazarr.qq <zhamely1982@gmail.com>
2026-03-14 18:11:34 +0200

    created finding users by name, and sorting by surname (#12)
    Co-authored-by: Demyd Titenko <128997514+Demyd06@users.noreply.github.com>

--- src/main/java/ua/knd12/service/impl/UserServiceImpl.java
+++ src/main/java/ua/knd12/service/impl/UserServiceImpl.java
@@ -3,10 +3,14 @@ import ua.knd12.model.User;
 import ua.knd12.service.UserService;
 
 import java.util.Arrays;
+import java.util.Locale;
+import java.text.Collator;
+import java.util.Comparator;
 
-public abstract class UserServiceImpl implements UserService {
-    protected static User[]initialArray;
-    private int counter = 0;
+abstract class UserServiceImpl implements UserService {
+    protected User[] initialArray;
+    protected int counter = 0;
+
 
     public UserServiceImpl(User[] initialArray) {
         this.initialArray = initialArray;
@@ -44,4 +48,37 @@ public abstract class UserServiceImpl implements UserService {
     public static User[] getInitialArray() {
         return initialArray;
     }
+
+    public User[] findByName(String query) {
+        if (initialArray == null || query == null) return new User[0]; //перевірка юзерів та запиту, якщо null то повертаємо 0, робиться для запобігання крашу коду
+        Locale uaLocale = new Locale("uk", "UA");
+        String lowerQuery = query.toLowerCase(uaLocale);
+
+        User[] result = new User[initialArray.length]; // тут зберігаємо вже відфільтрованих юзерів які співпадають з запитом
+
+        int foundUsers = 0; //кількість знайдених юзерів
+        for (User user : initialArray) {
+            if (user != null && user.getName() != null && user.getName().contains(lowerQuery)) {
+                result[foundUsers++] = user;
+            }
+
+        }return Arrays.copyOf(result, foundUsers); //повертаємо обрізаний масив result і foundUser
+    }
+       public User[] sortBySurname(){
+        Collator uaCollator = Collator.getInstance(new Locale("uk", "UA"));
+        uaCollator.setStrength(Collator.PRIMARY);// ігнор регістру
+
+        Arrays.sort(initialArray, 0, counter, new Comparator<User>() {
+            @Override
+            public int compare(User user1, User user2) {
+
+                if (user1 == null || user1.getSurname() == null) return 1;// переносить вперед
+                if (user2 == null || user2.getSurname() == null) return -1;// переносить не кінець
+
+                return uaCollator.compare(user1.getSurname(), user2.getSurname());
+            }
+        });
+           return Arrays.copyOf(initialArray, counter);
+    }
+
 }
